9da0f93efcc16b8729e47d42dc214f6d4c4782e0
rekredhead <rekredhead@gmail.com>
2023-04-02 17:56:47 +0530

    Initialize commit. Need to get images for category page

---
+++ itemsData.js
@@ -0,0 +1,80 @@
+export default {
+    category: {
+        food: [
+            {
+                id: 1,
+                title: "Spicy Chicken Sandwich",
+                price: 6.99,
+                description: "Juicy chicken breast seasoned with spicy sauce, served with lettuce and pickles on a toasted bun.",
+                image: 'path'
+            },
+            {
+                id: 2,
+                title: "Margherita Pizza",
+                price: 12.99,
+                description: "Classic Neapolitan-style pizza with tomato sauce, fresh mozzarella, and basil.",
+                image: 'path'
+            },
+            {
+                id: 3,
+                title: "Beef Tacos",
+                price: 9.99,
+                description: "Soft corn tortillas filled with seasoned ground beef, lettuce, cheese, and salsa.",
+                image: 'path'
+            }
+        ],
+        clothing: [
+            {
+                id: 4,
+                title: "Black Leather Jacket",
+                price: 199.99,
+                description: "A classic black leather jacket made from high-quality leather with a stylish design and comfortable fit.",
+                image: 'path'
+            },
+            {
+                id: 5,
+                title: "Blue Denim Jeans",
+                price: 59.99,
+                description: "A pair of blue denim jeans made from durable denim material with a slim-fit design and a stylish wash.",
+                image: 'path'
+            },
+            {
+                id: 6,
+                title: "White Cotton T-Shirt",
+                price: 24.99,
+                description: "A simple and comfortable white cotton t-shirt with a classic fit and a subtle logo on the chest.",
+                image: 'path'
+            }
+        ],
+        electronics: [
+            {
+                id: 7,
+                title: 'Samsung Galaxy S21',
+                price: 999,
+                description: 'A powerful and stylish smartphone from Samsung',
+                image: 'path'
+            },
+            {
+                id: 8,
+                title: 'Apple MacBook Air',
+                price: 1299,
+                description: 'A lightweight and powerful laptop from Apple',
+                image: 'path'
+            },
+            {
+                id: 9,
+                title: 'Sony WH-1000XM4',
+                price: 349,
+                description: 'Noise-cancelling headphones with excellent sound quality',
+                image: 'path'
+            },
+            {
+                id: 10,
+                title: 'DJI Mavic Air 2',
+                price: 799,
+                description: 'A high-quality drone with advanced features and excellent camera quality',
+                image: 'path'
+            }
+        ]
+    }
+};
