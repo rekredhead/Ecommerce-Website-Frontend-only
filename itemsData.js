bfab4d1ba9138d673cdeb8a4c3f9e9e92d545b71
rekredhead <rekredhead@gmail.com>
2023-04-05 16:33:15 +0530

    Converted product images to avif image format for better page loads

--- itemsData.js
+++ itemsData.js
@@ -6,21 +6,21 @@ export default {
                 title: "Spicy Chicken Sandwich",
                 price: 6.99,
                 description: "Juicy chicken breast seasoned with spicy sauce, served with lettuce and pickles on a toasted bun.",
-                image: 'itemSpicyChickenSandwich.png'
+                image: 'itemSpicyChickenSandwich.avif'
             },
             {
                 id: 2,
                 title: "Margherita Pizza",
                 price: 12.99,
                 description: "Classic Neapolitan-style pizza with tomato sauce, fresh mozzarella, and basil.",
-                image: 'itemMargheritaPizza.png'
+                image: 'itemMargheritaPizza.avif'
             },
             {
                 id: 3,
                 title: "Beef Tacos",
                 price: 9.99,
                 description: "Soft corn tortillas filled with seasoned ground beef, lettuce, cheese, and salsa.",
-                image: 'itemBeefTacos.png'
+                image: 'itemBeefTacos.avif'
             }
         ],
         clothing: [
@@ -29,21 +29,21 @@ export default {
                 title: "Black Leather Jacket",
                 price: 199.99,
                 description: "A classic black leather jacket made from high-quality leather with a stylish design and comfortable fit.",
-                image: 'itemBlackLeatherJacket.png'
+                image: 'itemBlackLeatherJacket.avif'
             },
             {
                 id: 5,
                 title: "Blue Denim Jeans",
                 price: 59.99,
                 description: "A pair of blue denim jeans made from durable denim material with a slim-fit design and a stylish wash.",
-                image: 'itemBlueDenimJeans.png'
+                image: 'itemBlueDenimJeans.avif'
             },
             {
                 id: 6,
                 title: "White Cotton T-Shirt",
                 price: 24.99,
                 description: "A simple and comfortable white cotton t-shirt with a classic fit and a subtle logo on the chest.",
-                image: 'itemWhiteCottonT-Shirt.png'
+                image: 'itemWhiteCottonT-Shirt.avif'
             }
         ],
         electronics: [
@@ -52,28 +52,28 @@ export default {
                 title: 'Samsung Galaxy S21',
                 price: 999,
                 description: 'A powerful and stylish smartphone from Samsung',
-                image: 'itemSamsungGalaxyS21.png'
+                image: 'itemSamsungGalaxyS21.avif'
             },
             {
                 id: 8,
                 title: 'Apple MacBook Air',
                 price: 1299,
                 description: 'A lightweight and powerful laptop from Apple',
-                image: 'itemAppleMacBookAir.png'
+                image: 'itemAppleMacBookAir.avif'
             },
             {
                 id: 9,
                 title: 'Sony WH-1000XM4',
                 price: 349,
                 description: 'Noise-cancelling headphones with excellent sound quality',
-                image: 'itemSonyWH-1000XM4.png'
+                image: 'itemSonyWH-1000XM4.avif'
             },
             {
                 id: 10,
                 title: 'DJI Mavic Air 2',
                 price: 799,
                 description: 'A high-quality drone with advanced features and excellent camera quality',
-                image: 'itemDJIMavicAir2.png'
+                image: 'itemDJIMavicAir2.avif'
             }
         ]
     }
